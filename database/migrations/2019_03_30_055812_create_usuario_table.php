cce5d2d8cc56192d4e95fe57c3272de3cffe17a8
danielmendez88 <daniboymendez@gmail.com>
2019-03-30 00:23:01 -0600

    cambios en la creacion de una migracion, agregar archivo htaccess

--- database/migrations/2019_03_30_055812_create_usuario_table.php
+++ database/migrations/2019_03_30_055812_create_usuario_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateUsuarioTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('usuario', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('usuario', 50);
+            $table->string('password', 100);
+            $table->string('nombre', 100);
+            $table->string('apaterno', 100);
+            $table->string('amaterno', 100);
             $table->timestamps();
         });
     }
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuario');
     }
 }
